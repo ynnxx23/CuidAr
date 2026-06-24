89ec393b3411b976ca3d8d73198ad640b66c3023
Yensel Villalba <yenselexequielvillalba@gmail.com>
2026-06-24 17:26:55 -0300

    base de datos y prisma

---
+++ backend/src/config/prisma.ts
@@ -0,0 +1,7 @@
+import { PrismaClient } from "../generated/prisma/client";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { env } from "./env";
+
+const adapter = new PrismaPg({ connectionString: env.DATABASE_URL });
+
+export const prisma = new PrismaClient({ adapter });
